f0b6704fda7ab4ee90b56dabd691c379e9d55d95
landon hickman <landonhickman12@gmail.com>
2021-07-09 16:00:08 -0600

    controllers complete

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,2 +1,48 @@
 class Api::PagesController < ApplicationController
+before_action :set_topic
+before_action :set_page, only: [:show, :update, :destroy]
+
+def index
+  render json: @topic.pages.all
 end
+
+def show
+  render json: @page
+end
+
+def create
+  @page = @topic.pages.new(page_params)
+  if @page.save
+    render json: @page
+  else
+    render json: @page.errors, status: 422
+  end
+end
+
+def update
+  if @page.update(page_params)
+    render json: @page
+  else
+    render json: @page.errors, status: 422
+  end
+end
+
+def destroy
+  render json: @page.destroy
+end
+
+private
+def set_topic
+  @topic = Topic.find(params[:topic_id])
+end
+
+def set_page
+  @page = @topic.pages.find(params[:id])
+end
+
+def page_params
+  params.require(:page).permit(:page_title, :image)
+end
+
+end
+
--- app/controllers/api/topics_controller.rb
+++ app/controllers/api/topics_controller.rb
@@ -1,2 +1,42 @@
 class Api::TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :update, :destroy]
+
+  def index
+    render json: Topic.all
+  end
+
+  def show
+    render json: @topic
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      render json: @topic
+    else
+      render json: @topic.errors, status: 422
+    end
+  end
+
+  def update
+    if @topic.update(topic_params)
+      render json: @topic
+    else
+      render json: @topic.errors, status: 422
+    end
+  end
+
+  def destroy
+    render json: @topic.destroy
+  end
+
+  private
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def topic_params
+    params.require(:topic).permit(:topic_name)
+  end
 end
